d7927540be19a3b321fcd96294af7e5faf0638c9
meduzzard <meduzzard@gmail.com>
2024-05-12 14:38:49 +0200

    connect MongoDB in app.js, allow localhost:3000 and localhost:3001 as origins

--- backend/app.js
+++ backend/app.js
@@ -4,10 +4,32 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var mongoose = require('mongoose');
+var mongoDB = "mongodb://127.0.0.1/trade-tracker";
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+var cors = require('cors');
+var allowedOrigins = ['http://localhost:3000', 'http://localhost:3001'];
+app.use(cors({
+  credentials: true,
+  origin: function(origin, callback){
+    // Allow requests with no origin (mobile apps, curl)
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin)===-1){
+      var msg = "The CORS policy does not allow access from the specified Origin.";
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
